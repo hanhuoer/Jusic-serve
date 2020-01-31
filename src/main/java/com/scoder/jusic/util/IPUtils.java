97630e575a0028c69b005d0a389586babf63516d
Incode <1206751678@qq.com>
2020-02-01 01:25:04 +0800

    :fire: 删去多余输出

--- src/main/java/com/scoder/jusic/util/IPUtils.java
+++ src/main/java/com/scoder/jusic/util/IPUtils.java
@@ -20,7 +20,6 @@ public class IPUtils {
 
     public static String getRemoteAddress(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
-        System.out.println("x-forwarded-for ip: " + ip);
         if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
             // 多次反向代理后会有多个ip值，第一个ip才是真实ip
             if (ip.indexOf(",") != -1) {
