0eaf32bbe512ed2247ec2b37560198427e59b839
Incode <1206751678@qq.com>
2020-02-01 22:26:38 +0800

    :bug: fix DC

--- src/main/java/com/scoder/jusic/handler/JusicWebSocketHandlerDecoratorFactory.java
+++ src/main/java/com/scoder/jusic/handler/JusicWebSocketHandlerDecoratorFactory.java
@@ -8,14 +8,14 @@ import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 /**
  * @author H
  */
-//@Component
-//public class JusicWebSocketHandlerDecoratorFactory implements WebSocketHandlerDecoratorFactory {
-//
-//    @Autowired
-//    private JusicWebSocketHandler jusicWebSocketHandler;
-//
-//    @Override
-//    public WebSocketHandler decorate(WebSocketHandler handler) {
-//        return this.jusicWebSocketHandler;
-//    }
-//}
+@Component
+public class JusicWebSocketHandlerDecoratorFactory implements WebSocketHandlerDecoratorFactory {
+
+    @Autowired
+    private JusicWebSocketHandler jusicWebSocketHandler;
+
+    @Override
+    public WebSocketHandler decorate(WebSocketHandler handler) {
+        return this.jusicWebSocketHandler;
+    }
+}
